41579418e0321e9ae7f00d94ed6c920641a1a1c2
lukewarm3 <lcao3@nd.edu>
2024-01-21 00:09:28 -0500

    useGetSuggestedUsers: fix the problem that firebase only limit 10 comparisons for "not-in"

--- src/hooks/useGetSuggestedUsers.js
+++ src/hooks/useGetSuggestedUsers.js
@@ -24,30 +24,41 @@ const useGetSuggestedUsers = () => {
             setIsLoading(true);
             try {
                 // get all users who are not the current user
-                const q = query(
-                    collection(db, "users"),
-                    where("uid", "not-in", [authUser.uid, ...authUser.following]),
-                    limit(3),
-                    orderBy("uid")
-                );
+                // const q = query(
+                //     collection(db, "users"),
+                //     where("uid", "not-in", [authUser.uid, ...authUser.following]),
+                //     limit(3),
+                //     orderBy("uid")
+                // );
+                // const querySnapshot = await getDocs(q);
+                // const users = [];
+                // querySnapshot.forEach((doc) => {
+                //     users.push({...doc.data(), id: doc.id});
+                // });
+
+                // get all users from the database
+                const allUsers = [];
+                const q = query(collection(db, "users"));
                 const querySnapshot = await getDocs(q);
-                const users = [];
                 querySnapshot.forEach((doc) => {
-                    users.push({...doc.data(), id: doc.id});
+                    allUsers.push({...doc.data(), id: doc.id});
+                });
+
+                // filter out the current user and the users the current user is following
+                const users = allUsers.filter((user) => {
+                    return user.uid !== authUser.uid && !authUser.following.includes(user.uid);
                 });
 
                 const luke = await getDoc(
                   doc(db, "users", "Xy5dx57dRBfVKhzogUun5AqN4bj2")
                 );
 
-                const find = users.find((user) => user.id === luke.id);
-
-                if (authUser.uid === luke.id || authUser.following.includes(luke.id) || find) {
-                    setSuggestedUsers(users);
+                if (authUser.uid === luke.id || authUser.following.includes(luke.id)) {
+                    setSuggestedUsers(users.slice(0, 3));
                 }
                 else {
                     users.unshift({...luke.data(), id: luke.id});
-                    setSuggestedUsers(users);
+                    setSuggestedUsers(users.slice(0, 3));
                 }
             } catch (error) {
                 showToast("Error", error.message, "error");
